8ea52cdcddd4059b3cdcebb76e3774220d9359cc
luisaschivoni <162350432+luisaschivoni@users.noreply.github.com>
2024-10-18 14:39:49 -0300

    l

---
+++ perguntas.js
@@ -0,0 +1,30 @@
+criaCartao(
+    'One Direction',
+    'em que ano a banda de separou?',
+    '2016'
+)
+criaCartao(
+    'Justin Bieber',
+    'qual a musica do Bieber foi a mais tocada no ano de 2024?',
+    'What Do You Me'
+)
+criaCartao(
+    'Justin Bieber',
+    'qual musica o bieber admite ter sido obrigado a gravar pela sua gravadora?',
+    'Beuty And The Beat'
+)
+criaCartao(
+    'One Direction',
+    'qual é o membro mais novo da banda?',
+    'Harry Style'
+)
+criaCartao(
+    'onde direction',
+    'quantos singles a banda tem?',
+    '17 singles'
+)
+criaCartao(
+    'justin bieber',
+    'quantos singles bieber tem?',
+    '39 singles'
+)
